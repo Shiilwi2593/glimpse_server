5264d38ae164ae34bf5f9e17b5b37f462ea8bdd8
graylwi2593 <graylwi2593@gmail.com>
2024-08-25 23:10:35 +0700

    Add user.routes.js

--- server.js
+++ server.js
@@ -44,3 +44,8 @@ mongoose.connect("mongodb+srv://graylwi2593:Gray2592003@glimpse.cl9pe.mongodb.ne
         console.error("Connection to mongoDB failed");
     });
 
+    
+
+app.use('/api/users', userRoutes);
+
+//this is new
