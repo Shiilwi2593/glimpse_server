b500bbe01510d6f0c6e87dfbf4305f9d954c5083
graylwi2593 <graylwi2593@gmail.com>
2024-08-26 16:18:21 +0700

    new user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,17 +1,48 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: {type: String, unique: true, required: true},
-    email: {type: String, required: true, unique:true},
-    password: {type: String, required: true, unique: true},
-    friends: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-    location:{
-        latitude:{type: Number},
-        longitude:{type: Number},
-        updateAt: {type: Date}
+    username: {
+        type: String,
+        unique: true,
+        required: true
     },
-    createAt: {type: Date, default: Date.now},
-    updateAt: {type: Date, default: Date.now}      
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        default: 'https://i.ibb.co/jggwqDf/defaultavatar.jpg'
+    },
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    location: {
+        latitude: { type: Number },
+        longitude: { type: Number },
+        updateAt: { type: Date }
+    },
+    createAt: {
+        type: Date,
+        default: Date.now
+    },
+    updateAt: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['unverified', 'verified', 'account_locked'],
+        default: 'unverified'
+    }
 })
 
 module.exports = mongoose.model('User', UserSchema)
